c889ddeb28e6541c363030a8cb249763d8caf1eb
Samuel T. Croker <samuel.croker@gmail.com>
2017-02-27 11:34:56 -0500

    summary 3

--- abstractIML.R
+++ abstractIML.R
@@ -1,8 +1,7 @@
 
-print("Look at columns from dataset provided by SAS")
-fmla <- "MPG_Highway ~ EngineSize + Cylinders + Horsepower + Weight + Wheelbase + Length"
 
 fit <- lm(df,formula=fmla)
 
 fit.diag  <- cbind(fit$residuals,fit$fitted.values)
 
+summary(fit.diag)  
