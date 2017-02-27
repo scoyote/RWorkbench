394a48d32f93f2ca20072407710afa6f6a9bdf34
Samuel T. Croker <samuel.croker@gmail.com>
2017-02-27 08:34:00 -0500

    switched cars to df

--- sasimlHW.R
+++ sasimlHW.R
@@ -1,7 +1,7 @@
 
 print("Look at columns from dataset provided by SAS")
 
-fit <- lm(cars_lm,formula=MPG_Highway ~ EngineSize + Cylinders + Horsepower + Weight + Wheelbase + Length)
+fit <- lm(df,formula=MPG_Highway ~ EngineSize + Cylinders + Horsepower + Weight + Wheelbase + Length)
 
 fit.diag  <- cbind(fit$residuals,fit$fitted.values)
 
